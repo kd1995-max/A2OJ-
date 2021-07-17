988c73f663654d8400c2a929d9c0653de8b14d85
kd1995-max <k.d1995@outlook.com>
2021-07-18 02:57:03 +0530

    ok

--- Codeforces Rating < 1300/A_Young_Physicist.cpp
+++ Codeforces Rating < 1300/A_Young_Physicist.cpp
@@ -6,10 +6,24 @@ int main(int argc, char const *argv[])
 {
     int T;
     cin >> T;
-    int sum = 0;
+    int sum_a = 0 , sum_b = 0 , sum_c = 0;
     while(T--)
     {
-        
+        int a;
+        cin >> a;
+        sum_a += a;
+        int b;
+        cin >> b;
+        sum_b += b;
+        int c;
+        cin >> c;
+        sum_c += c;
     }
+
+    if(sum_a == 0 and sum_b == 0 and sum_c == 0)
+    cout << "YES" <<endl;
+    else
+    cout << "NO" <<endl;
+
     return 0;
 }
