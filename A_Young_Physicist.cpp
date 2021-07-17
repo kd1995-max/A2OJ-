64b120d1da71f0c9e51ac373928ba83a9bf75d48
kd1995-max <k.d1995@outlook.com>
2021-07-18 02:24:03 +0530

    inti

---
+++ A_Young_Physicist.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int T;
+    cin >> T;
+    int sum = 0;
+    while(T--)
+    {
+        
+    }
+    return 0;
+}
