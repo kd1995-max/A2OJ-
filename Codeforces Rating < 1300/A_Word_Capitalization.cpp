087efbb7dba3f6f0f0323b13657878c116c59ecc
kd1995-max <k.d1995@outlook.com>
2021-07-20 00:52:44 +0530

    ok

---
+++ Codeforces Rating < 1300/A_Word_Capitalization.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string str;
+    cin >> str;
+    str[0] = toupper(str[0]);
+    cout << str;
+    return 0;
+}
